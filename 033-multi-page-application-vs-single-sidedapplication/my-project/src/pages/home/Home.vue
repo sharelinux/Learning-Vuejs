f92f73c169a5b64485cb8e5160cf2a1e068ae102
Anjie Li <sharelinuxs@gmail.com>
2021-05-22 23:56:20 +0800

    #033 多页面应用vs单页面应用

---
+++ 033-multi-page-application-vs-single-sidedapplication/my-project/src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="home">Home</div>
+    <router-link to="/list">列表页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style type="text/css">
+    .home {
+        font-size: 18px;
+    }
+</style>
