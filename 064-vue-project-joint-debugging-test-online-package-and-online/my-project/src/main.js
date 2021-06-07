67049460a4398aa33aa06cdcc357366f8790746a
Anjie Li <sharelinuxs@gmail.com>
2021-06-07 22:31:04 +0800

    #064 Vue项目联调测试上线-打包上线

---
+++ 064-vue-project-joint-debugging-test-online-package-and-online/my-project/src/main.js
@@ -0,0 +1,32 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastClick from 'fastclick'
+import store from './store'
+import 'babel-polyfill'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// import style (<= Swiper 5.x)
+import 'swiper/css/swiper.css'
+
+// import style (>= Swiper 6.x)
+// import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+// import 'swiper/swiper-bundle.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
---
+++ 064-vue-project-joint-debugging-test-online-package-and-online/my-project/src/pages/detail/Detail.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <detail-banner
+      :sightName="sightName"
+      :bannerImg="bannerImg"
+      :bannerImgs="gallaryImgs"
+    ></detail-banner>
+    <detail-header></detail-header>
+    <div class="content">
+        <detail-list :list="list"></detail-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
+import DetailList from './components/List'
+import axios from 'axios'
+
+export default {
+  name: 'Detail',
+  components: {
+    DetailBanner,
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      categoryList: [],
+      list: []
+    }
+  },
+  methods: {
+    getDetailInfo () {
+      // 方式1 参数拼接的方式请求
+      // axios.get('/api/detail.json?id=' + this.$route.params.id)
+
+      // 方式2 params参数方法
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handelGetDataSucc)
+    },
+    handelGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        // console.log(data)
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.list = data.categoryList
+      }
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .content
+    height: 50rem
+</style>
